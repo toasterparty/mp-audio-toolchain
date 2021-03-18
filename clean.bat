4db8e3535201d0770884fcf8537b9ff13aee676d
Blake Spangenberg <bspangenberg@insteon.com>
2021-03-18 09:32:20 -0700

    added clean command

---
+++ clean.bat
@@ -0,0 +1,32 @@
+@echo off
+set SCRIPT_DIR=%cd%
+
+del *.iso
+
+if exist %SCRIPT_DIR%\prime rmdir %SCRIPT_DIR%\prime /s /q
+
+cd %SCRIPT_DIR%\tools\audacity
+del *.dll
+del *.exe
+
+cd %SCRIPT_DIR%\tools\gcit
+del *.dll
+del *.exe
+del *.ini
+del *.txt
+
+cd %SCRIPT_DIR%\tools\paktool
+del *.dll
+del *.exe
+del *.ini
+del *.txt
+
+cd %SCRIPT_DIR%\tools\urde
+del *.dll
+del *.exe
+del *.ini
+del *.txt
+del *.pdb
+
+cd %SCRIPT_DIR%\tools
+del .wget-hsts
